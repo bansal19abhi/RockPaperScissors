2fdd5ddfcb17e667a513462c88236ad019ceef4d
Abhishek Bansal <abbansal1995@gmail.com>
2018-11-29 13:54:26 -0500

    abhi added soem nonsense

--- app/src/main/java/com/example/abhishekbansal/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/abhishekbansal/rockpaperscissors/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.abhishekbansal.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // this is our first project!
+        // this is our first project!--->88888888888888884764565465465654
+        Log.d("ABHI", "here is a comment");
+
 
         // added a comment from abhi's computer
 

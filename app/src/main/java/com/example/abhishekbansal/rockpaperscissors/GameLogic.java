91098b2d888d228d4b575d707ed0e9aa635c3d5d
tejas <jtejas10@gmail.com>
2018-12-03 10:58:15 -0500

    added game logic

---
+++ app/src/main/java/com/example/abhishekbansal/rockpaperscissors/GameLogic.java
@@ -0,0 +1,200 @@
+package com.example.abhishekbansal.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameLogic extends AppCompatActivity {
+
+    String Rock = "Rock";
+    String Paper = "Paper";
+    String Scissor = "Scissor";
+    String Lizard = "Lizard";
+    String Spock = "Spock";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_logic);
+    }
+
+
+// similar
+
+    if(player1 =="Paper"&&player2 =="Paper")
+
+    {
+        System.out.println("It's a Tie");
+    }
+    else if(player1 =="Rock"&&player2 =="Rock")
+
+    {
+        System.out.println("It's a Tie");
+    }
+    else if(player1 =="Scissors"&&player2 =="Scissors")
+
+    {
+        System.out.println("It's a Tie");
+    }
+    else if(player1 =="Lizard"&&player2 =="Lizard")
+
+    {
+        System.out.println("It's a Tie");
+    }
+    else if(player1 =="Spock"&&player2 =="Spock")
+
+    {
+        System.out.println("It's a Tie");
+    }
+// Rock
+    else if(player1 =="Rock"&&player2 =="Paper")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Rock"&&player2 =="Lizard")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Rock"&&player2 =="Spock")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Rock"&&player2 =="Scissors")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+// Paper
+
+    else if(player1 =="Paper"&&player2 =="Rock")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Paper"&&player2 =="Lizard")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Paper"&&player2 =="Spock")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Paper"&&player2 =="Scissors")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+// Scissors
+
+    else if(player1 =="Scissors"&&player2 =="Paper")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Scissors"&&player2 =="Rock")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Scissors"&&player2 =="Lizard")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Scissors"&&player2 =="Spock")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+// Spock
+
+    else if(player1 =="Spock"&&player2 =="Scissors")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Spock"&&player2 =="Paper")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Spock"&&player2 =="Rock")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Spock"&&player2 =="Lizard")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+// Lizard
+
+    else if(player1 =="Lizard"&&player2 =="Spock")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+    else if(player1 =="Lizard"&&player2 =="Scissors")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Lizard"&&player2 =="Paper")
+
+    {
+        System.out.println("Player 1 Wins");
+    }
+    else if(player1 =="Lizard"&&player2 =="Rock")
+
+    {
+        System.out.println("Player 2 Wins");
+    }
+}
+
+//
+//    else if (player1 == 'Rock' and player2 == 'Rock'):
+//    print "it's a it's a Tie"
+
+//    else if (player1 == 'Scissors' and player2 == 'Paper'):
+//    print "Player 1 wins."
+//
+//    else if (player2 == 'Scissors' and player2 == 'Scissors'):
+//    print "it's a Tie"
+//
+//    else if (player1 == 'Paper' and player2 == 'Paper'):
+//    print "it's a Tie"
+//
+//    else if (player1 == 'Paper' and player2 == 'Scissors'):
+//    print "Player 2 wins."
+//
+//    else if (player1 == 'Rock'and player2 == 'Paper'):
+//    print "Player 2 wins."
+//
+//    else if (player1 == 'Paper' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//
+//    else if (player1 == 'Scissors' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//// hello
+//            else if (player1 == 'Paper' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//
+//            else if (player1 == 'Scissors' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//
+//            else if (player1 == 'Paper' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//
+//            else if (player1 == 'Scissors' and player2 == 'Rock'):
+//    print "Player 2 wins."
+//
+//
+//
+//            else:
+//    print "This is not a valid object selection."
+
